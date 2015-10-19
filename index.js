4d57178361938065e2c9b8e4bc45b2800a91d9a1
Patrick Walsh <patrick.nilsen.walsh@gmail.com>
2015-10-19 22:37:40 +1300

    added signatures to everything

--- index.js
+++ index.js
@@ -8,126 +8,67 @@
   'use strict';
 
   var Re = {};
-  /**
-   * Creates a function that calls several functions with the same arguments.
-   * https://github.com/ramda/ramda/issues/986
-   * @function juxt
-   * @param {Array} array of functions to apply to the object in parallel
-   * @param {Object}
-   */
-  Re.juxt = R.useWith(R.ap, R.identity, R.of);
 
-  /**
-   * The complement of R.Empty
-   * @type {Function}
-   */
+  // @sig a -> Boolean
   Re.isNotEmpty = R.complement(R.isEmpty);
-
-  /**
-   * The complement of R.isNil
-   * @type {Function}
-   */
   Re.isNotNil = R.complement(R.isNil);
-
-  /**
-   * isEmpty checks the "length" attr of an obj, which a POJO doesn't have.
-   * @type {Function}
-   */
   Re.isEmptyObj = R.compose(R.isEmpty, Object.keys);
-
-  /**
-   * The complement of isEmptyObj
-   * @type {Function}
-   */
   Re.isNotEmptyObj = R.complement(Re.isEmptyObj);
-
-
-  /**
-   * Checks whether a String or Array is undefined, null, or "empty"
-   * @param {String|Array}
-   * @type {Function}
-   */
   Re.isNilOrEmpty = R.anyPass([R.isNil, R.isEmpty]);
-
-  /**
-   * Converse of isNilOrEmpty
-   * @param {String|Array}
-   * @type {Function}
-   */
   Re.isNotNilOrEmpty = R.complement(Re.isNilOrEmpty);
-
-  /**
-   * Checks whether an Object is undefined, null, or has no keys
-   * @param {Object}
-   * @type {Function}
-   */
   Re.isNilOrEmptyObj = R.anyPass([R.isNil, Re.isEmptyObj]);
-
-  /**
-   * Converse of isNilOrEmptyObj
-   * @param {Object}
-   * @type {Function}
-   */
   Re.isNotNilOrEmptyObj = R.complement(Re.isNilOrEmptyObj);
 
-
-  /**
-   * The inverse of R.merge
-   *
-   * R.merge(a, b) --> own properties of b overwrites properties of a
-   * mergeRight(a, b) --> own properties of a overwrites properties of b
-   *
-   * @func
-   * @memberOf Re
-   * @category Function
-   * @param {Object} a
-   * @param {Object} b
-   * @return {Object} a merged into b
-   * @see R.merge
-   * @see R.flip
-   */
+  // @sig {k: v} -> {k: v} -> {k: v}
   Re.mergeRight = R.flip(R.merge);
 
-  Re.trace = function (message, fn) {
-    return function (val) {
-      if (fn) {
-        console.log(message, fn(val), val)
-      } else {
-        console.log(message, val)
-      }
-      return val
-    }
-  };
-  Re.log = Re.trace;
-
   Re.toArray = R.invoker(0, 'toArray');
+  Re.toNumber = x => Number(x);
+  Re.toString = x => Number(x);
 
-  Re.effect = R.curryN(2, function (fn, x) {
+  // @sig a -> (a -> b) -> b
+  Re.applyTo = R.curryN(2, (obj, fn) => fn(obj));
+  // @sig a -> [(a -> b)] -> [b]
+  Re.rmap = R.curryN(2, function rmap (obj, fns) {
+    return R.map(Re.applyTo(obj), fns)
+  });
+  // @sig [(a -> b)] -> a -> [b]
+  Re.juxt = R.flip(Re.rmap);
+
+  // @sig a -> a
+  Re.effect = R.curryN(2, function effect (fn, x) {
     fn(x);
     return x
   });
 
+  // @sig String -> (a -> a)
+  Re.trace = (msg) => Re.effect(val => console.log(msg, val));
+  // @sig String -> (a -> b) -> (a -> a)
+  Re.traceWith = (msg, fn) => Re.effect(val => console.log(msg, fn(val)));
+  Re.log = Re.trace;
+  Re.logWith = Re.traceWith;
+
+
   /**
-   * A collection promise-enabled control flow methods
+   * rsvp is a collection promise-enabled control flow functions
    */
   var rsvp = {};
   Re.rsvp = rsvp;
 
-  /**
-   * A version of juxt that works with composeP
-   * @sig [(* -> *)] -> * -> Ember.RSVP.all([*])
-   * @func
-   */
-  rsvp.parallel = R.curryN(2, function (fns, x) {
+  rsvp.compose = R.composeP;
+  rsvp.pipe = R.pipeP;
+
+  // @sig [(a -> b)] -> a -> [b]
+  rsvp.parallel = R.curryN(2, function parallel (fns, x) {
     return rsvp.all(Re.juxt(fns))(x)
-  })
+  });
 
   /**
    * Identical to rsvp.parallel, except that instead of a list,
    * you pass an object with the functions as the keys
    *
    * @func
-   * @sig {k: (v -> v)} -> * -> Ember.RSVP.promise({k: v})
+   * @sig {k: (a -> b)} -> a -> Promise({k: b})
    * @param {Object} transformations The object specifying transformation functions to invoke in parallel
    * @param {Object} object The object to be transformed
    * @returns {Ember.RSVP.hash} The results of invoking the transformations with the object
@@ -135,42 +76,29 @@
    */
   rsvp.parallelHash = R.curryN(2, function parallelHash (transformations, object) {
     return R.compose(
-        Ember.RSVP.hash,
-        R.mapObj(fn => fn(object))
+      Ember.RSVP.hash,
+      R.mapObj(fn => fn(object))
     )(transformations)
   });
 
-
-  rsvp.all = R.curryN(1, function (fn) {
+  // Wraps the result of calling fn in Ember.RSVP.all
+  rsvp.all = R.curryN(1, function all (fn) {
     return R.compose(
-        Ember.RSVP.all,
-        fn
+      Ember.RSVP.all,
+      fn
     )
   });
 
-  rsvp.hash = R.curryN(1, function (fn) {
+  // Wraps the result of calling fn in Ember.RSVP.hash
+  rsvp.hash = R.curryN(1, function hash (fn) {
     return R.compose(
-        Ember.RSVP.hash,
-        fn
+      Ember.RSVP.hash,
+      fn
     )
   });
 
-  /**
-   * Shorthand for `rsvp.hash(R.compose(/* fns... *\/))`
-   */
-  rsvp.compseHash = function composeHash () {
-    return rsvp.hash(R.apply(R.compose, arguments))
-  };
-
-
-  /**
-   * A version of map that works with composeP
-   * @sig (* -> *) -> * -> Ember.RSVP.all([*])
-   * @function
-   * @param {Function} fn a function to apply to all items in list
-   * @param {Array} list an array or items to apply the function to
-   */
-  rsvp.map = R.curryN(2, function (fn, list) {
+  // @sig (a -> b) -> [a] -> Promise([b])
+  rsvp.map = R.curryN(2, function map (fn, list) {
     return rsvp.all(R.map(fn))(list)
   });
 
@@ -181,7 +109,7 @@
    * @param {Function} fn a function to apply to all items in list
    * @param {Array} list an array or items to apply the function to
    */
-  rsvp.filter = R.curryN(2, function (fn, list) {
+  rsvp.filter = R.curryN(2, function filter (fn, list) {
     return rsvp.all(R.filter(fn))(list)
   });
 
@@ -193,133 +121,73 @@
    * @param {*} x
    * @returns {Ember.RSVP.Promise} Resolves to x
    */
-  rsvp.effect = R.curryN(2, function (fn, x) {
+  rsvp.effect = R.curryN(2, function effect (fn, x) {
     return fn(x).then(R.always(x))
   });
 
-
-  rsvp.compose = R.composeP;
-  rsvp.pipe = R.pipeP;
-
-
-  /**
-   * Methods to use with with Ember objects
-   */
+  /* Ember-specific functions */
   if (Ember) {
-
-    /**
-     * An implementation of Ramda that is compatible with Ember getters/setters
-     * @type {{}}
-     */
     var E = {};
     Re.E = E;
     Re.Ember = E;
 
-    /**
-     * R.prop for Ember objects
-     * @sig a -> b -> Ember.get(b, a)
-     * @param {String} name of attribute to get
-     * @param {Object} object to get the attribute from
-     * @return {*}
-     */
+    /* Re-implement Ramda functions to work with Ember getters/setters*/
+    // @sig k -> Object -> v
     E.prop = R.flip(Ember.get);
     E.get = E.prop;
 
-    /**
-     * Pluck over an array of ember objects
-     * @param p
-     * @param list
-     */
-    E.pluck = R.curryN(2, function epluck (p, list) {
+    // @sig k -> [Object] -> [v]
+    E.pluck = R.curryN(2, function pluck (p, list) {
       return R.map(E.get(p), list);
     });
 
-    /**
-     * @see R.propSatisfies
-     */
+    // @sig (a -> Boolean) -> String -> {String: a} -> Boolean
     E.propSatisfies = R.curryN(3, function propSatisfies (pred, name, obj) {
       return pred(Ember.get(obj, name))
     });
 
-    /**
-     * @see R.propEq
-     */
-    E.propEq = R.curryN(3, function propEq (val, name, obj) {
+    // @sig String -> a -> Object -> Boolean
+    E.propEq = R.curryN(3, function propEq (name, val, obj) {
       return E.propSatisfies(R.equals(val), name, obj)
     });
 
-    /**
-     * @see R.eqProps
-     */
+    // @sig String -> Object -> Object -> Boolean
     E.eqProps = R.curryN(3, function eqProps (prop, obj1, obj2) {
       return R.equals(E.get(prop, obj1), E.get(prop, obj2))
     });
 
-    /**
-     * Invokes `object.set(keyName, value)` then returns the object
-     * @func
-     * @sig String -> * -> Object -> Object
-     * @param {String} keyName
-     * @param {*} value
-     * @param {Object} object
-     * @returns {Object}
-     */
+    /* Custom Ember methods */
+
+    // @sig DS.Model -> Ember.RSVP.Promise
+    E.save = R.invoker(0, 'save');
+    // @sig DS.Model -> DS.Model
+    E.clone = R.invoker(0, 'clone');
+    // @sig a -> * -> a
+    E.setProperties = R.flip(Ember.setProperties);
+    // @sig String -> * -> a -> a
     E.set = R.curryN(3, function set (keyName, value, object) {
       Ember.set(object, keyName, value);
       return object
     });
 
-    /**
-     * Calls `save` on the calling model
-     * @func
-     * @sig DS.Model -> Ember.RSVP.Promise
-     * @param {DS.Model} model The model instance to save
-     * @returns {Ember.RSVP.Promise} Resolves to the supplied model after having been saved
-     */
-    E.save = R.invoker(0, 'save');
-
-
-    /**
-     * Calls `save` on an object
-     * @func
-     * @sig DS.Model -> DS.Model
-     * @param {DS.Model} model The model instance to clone
-     * @returns {DS.Model} A cloned instance of the model
-     */
-    E.clone = R.invoker(0, 'clone');
-
-
-    /**
-     * @func
-     * @sig Object -> * -> *
-     * @param {Object} pojo of objects to pass to setProperties
-     * @param {*} object to call setProperties on
-     */
-    E.setProperties = R.flip(Ember.setProperties)
-
-    /**
-     *
-     */
-    E.linkRelated = R.curryN(2, function (parent, related) {
+    // @sig a -> b -> Promise(b)
+    E.linkRelated = R.curryN(2, function linkRelated (parent, related) {
       return rsvp.effect(b => parent.linkRelated(b), related)
-    })
-
-    E.linkManyRelated = R.curryN(2, function (theOne, theMany) {
+    });
+    // @sig a -> [b] -> PromiseArray([b])
+    E.linkManyRelated = R.curryN(2, function linkManyRelated (theOne, theMany) {
       return rsvp.map(E.linkRelated(theOne), theMany)
-    })
+    });
 
-    E.linkCustomRelated = R.curryN(3, function (relatedName, a, b) {
+    // @sig String -> a -> b -> Promise(b)
+    E.linkCustomRelated = R.curryN(3, function linkCustomRelated (relatedName, a, b) {
       return rsvp.effect(b => a.linkRelated(b, relatedName), b)
-    })
+    });
 
-
-    /**
-     * String -> DS.Model -> Ember.RSVP.Promise([DS.Model])
-     */
-    E.findAllRelated = R.curryN(2, function (relatedModelName, model) {
+    // @sig String -> a -> PromiseArray([b])
+    E.findAllRelated = R.curryN(2, function findAllRelated (relatedModelName, model) {
       return model.get('store').findAllRelated(model, relatedModelName);
     })
-
   }
 
   if (typeof exports === 'object') {

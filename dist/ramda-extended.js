7789c286cd646e6350bd944beccb88f8b58eeaeb
Patrick Walsh <patrick.nilsen.walsh@gmail.com>
2015-10-29 15:05:23 +1300

    Remove /dist from .gitignore

---
+++ dist/ramda-extended.js
@@ -0,0 +1,66 @@
+/**
+ * A collection of utilities
+ */
+
+(function () {
+  var root = this;
+  var R = require('ramda');
+
+  // @sig a -> Boolean
+  R.isUndefined = function (x) {return typeof x === 'undefined'};
+  R.isNotEmpty = R.complement(R.isEmpty);
+  R.isNotNil = R.complement(R.isNil);
+  R.isEmptyObj = R.compose(R.isEmpty, Object.keys);
+  R.isNotEmptyObj = R.complement(R.isEmptyObj);
+  R.isNilOrEmpty = R.anyPass([R.isNil, R.isEmpty]);
+  R.isNotNilOrEmpty = R.complement(R.isNilOrEmpty);
+  R.isNilOrEmptyObj = R.anyPass([R.isNil, R.isEmptyObj]);
+  R.isNotNilOrEmptyObj = R.complement(R.isNilOrEmptyObj);
+
+  // @sig {k: v} -> {k: v} -> {k: v}
+  R.mergeRight = R.flip(R.merge);
+
+  R.toArray = R.invoker(0, 'toArray');
+  R.toJSON = R.invoker(0, 'toJSON');
+  R.toNumber = Number;
+  R.toString = String;
+
+  // @sig a -> (a -> b) -> b
+  R.applyTo = R.curryN(2, function (obj, fn) { return fn(obj) });
+  // @sig a -> [(a -> b)] -> [b]
+  R.rmap = R.curryN(2, function rmap (obj, fns) {
+    return R.map(R.applyTo(obj), fns)
+  });
+  // @sig [(a -> b)] -> a -> [b]
+  R.juxt = R.flip(R.rmap);
+
+  // @sig a -> a
+  R.effect = R.curryN(2, function effect (fn, x) {
+    fn(x);
+    return x
+  });
+
+  // @sig a -> a
+  R.log = R.effect(function (val) {console.log(val)});
+  // @sig (a -> b) -> a -> a
+  R.logWith = function (fn) {
+    return R.effect(function (val) {console.log(fn(val))});
+  };
+  // @sig String -> (a -> a)
+  R.trace = function (msg) {
+    return R.effect(function (val) {console.log(msg, val)});
+  };
+  // @sig String -> (a -> b) -> (a -> a)
+  R.traceWith = function (msg, fn) {
+    return R.effect(function (val) {console.log(msg, fn(val))});
+  };
+
+  if (typeof exports === 'object') {
+    module.exports = R;
+  } else if (typeof define === 'function' && define.amd) {
+    define(function () { return R; });
+  } else {
+    root.R = R;
+  }
+
+}.call(this));
